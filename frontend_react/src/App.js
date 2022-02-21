dc83d074ba6e493d8f8fe8a8314bfb62392810d1
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-02-21 01:29:50 -0500

    clean up

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components"
+
+import './App.scss'
 const App = () => {
   return (
-      <div className="App">
+      <div className="app">
         <Navbar />
         <Header />
         <About />
