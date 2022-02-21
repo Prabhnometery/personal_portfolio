205d244a91dafde6e6286bd98dabfd61aaf69ada
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-02-20 22:55:00 -0500

    added all req schemas and react setup

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,30 +1,30 @@
 export default {
-    name: 'testimonials',
-    title: 'Testimonials',
+    name:'testimonials',
+    title:'Testimonials',
     type: 'document',
-    fields: [
-        {
-            name: 'name',
-            title: 'Name',
-            type: 'string',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
         },
         {
-            name: 'company',
-            title: 'Company',
-            type: 'string',
+            name:'company',
+            title:'Company',
+            type:'string'
         },
         {
-            name: 'imgurl',
-            title: 'Imgurl',
+            name:'imgurl',
+            title:'ImgUrl',
             type: 'image',
             options: {
-                hotspot: true,
-            }
-        },
+              hotspot: true,
+            },
+        },  
         {
-            name: 'feedback',
-            title: 'Feedback',
-            type: 'string',
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
         }
     ]
 }
