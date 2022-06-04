789d522f923a3e6d130a299f72e333d4a33d65cc
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-06-04 13:42:55 -0400

    added testimonial to nav dot

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "skills", "work", "testimonials", "contact"].map(
+      {["home", "about", "skills", "work", "testimonial", "contact"].map(
         (item, index) => (
           <a
             href={`#${item}`}
